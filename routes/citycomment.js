490621ecf80270ab3a16df24c1e0d6682d9bf179
Robert de Grijs <rdgrijs@hotmail.com>
2019-11-06 16:27:18 +0100

    add new route city comment

--- routes/citycomment.js
+++ routes/citycomment.js
@@ -1,4 +1,18 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
+const City = require("../models/city");
+
+// NEW
+router.get("/new", function(req, res) {
+  res.render("citycomments/new");
+});
+
+// CREATE
+
+// EDIT
+
+// UPDATE
+
+// DELETE
 
 module.exports = router;
